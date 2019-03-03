5b6eb2806972830aebd3c8cbfb664b1b1544faaf
Niki Micallef <nikmic1994@hotmail.com>
2019-03-03 19:58:10 +0100

    - Updated content - Removed contact sheet and selector - Added link to skype - Formatting changes

--- javaScript.js
+++ javaScript.js
@@ -2,32 +2,6 @@
  * Created by Niki on 16/08/2016.
  */
 
-function contactButtonPress() {
-    $("#topTable").fadeOut(500);
-    $("#bottomTable").fadeOut(500, function () {
-        $("#contact").fadeIn(500);
-        if ($(window).width() >= 974) {
-            $("#parent").css("padding-right", "17px");
-        }
-        sidenavHeight();
-    });
-
-    document.getElementById("navLeft").style.color = "#DCDCDC";
-    document.getElementById("navRight").style.color = "#424242";
-}
-
-function resumeButtonPress() {
-    $("#contact").fadeOut(500, function () {
-        $("#topTable").fadeIn(500);
-        $("#bottomTable").fadeIn(500);
-        $("#parent").css("padding-right", "0px");
-        sidenavHeight();
-    });
-
-    document.getElementById("navLeft").style.color = "#424242";
-    document.getElementById("navRight").style.color = "#DCDCDC";
-}
-
 function sidenavHeight() {
     if ($(window).width() >= 974) {
         if ($('#contact').is(":visible")) {
